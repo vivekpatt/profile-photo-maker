76034195db3e604b1862ca8c47e32a6746b998ba
Vivek Patt <vivekrpatt@gmail.com>
2022-12-15 01:47:57 +0530

    Added logic to mask the images to add backdrop in the background.

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 var jimp = require("jimp");
 var DP_IN = "dp.png";
 var DP_OUT = "dp-circle.png";
+var BACKDROP = "backdrop.png";
 
 async function main() {
   const dp = await jimp.read("./input/" + DP_IN);
-  dp.quality(60)
+  const dp_mask = dp.clone();
+  const bd = await jimp.read("./input/" + BACKDROP);
+
+  //*   Create mask from input DP
+  dp_mask.color([{ apply: "saturate", params: [5] }]);
+  dp_mask
+    .pixelate(5)
+    .blur(5)
+    .scan(
+      0,
+      0,
+      dp_mask.bitmap.width,
+      dp_mask.bitmap.height,
+      function (x, y, idx) {
+        let red = this.bitmap.data[idx + 0];
+        let green = this.bitmap.data[idx + 1];
+        let blue = this.bitmap.data[idx + 2];
+
+        let threshold = 3;
+
+        if (
+          Math.abs(red - green) > threshold &&
+          Math.abs(red - blue) > threshold &&
+          Math.abs(green - blue) > threshold
+        ) {
+          this.bitmap.data[idx + 0] = 0;
+          this.bitmap.data[idx + 1] = 0;
+          this.bitmap.data[idx + 2] = 0;
+        }
+      }
+    );
+
+  bd.resize(dp_mask.bitmap.width, dp_mask.bitmap.height).mask(dp_mask);
+
+  dp.blit(bd, 0, 0)
+    .quality(60)
     .circle()
     .write("./output/" + DP_OUT);
 }
